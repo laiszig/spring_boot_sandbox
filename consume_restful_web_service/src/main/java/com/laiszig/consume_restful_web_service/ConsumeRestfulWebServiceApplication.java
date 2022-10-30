f9acbe3ce6741b05e6d239336c1e5ff3de7abe2e
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-10-30 11:33:41 -0300

    add new project

---
+++ consume_restful_web_service/src/main/java/com/laiszig/consume_restful_web_service/ConsumeRestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.consume_restful_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeRestfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumeRestfulWebServiceApplication.class, args);
+    }
+
+}
