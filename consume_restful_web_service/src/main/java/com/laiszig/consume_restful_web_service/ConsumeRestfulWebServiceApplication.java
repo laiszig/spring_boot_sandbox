2896a25fdaf64053a8737afd24332382b1f5facf
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-10-30 14:22:56 -0300

    create project capable of consuming a RESTful service

--- consume_restful_web_service/src/main/java/com/laiszig/consume_restful_web_service/ConsumeRestfulWebServiceApplication.java
+++ consume_restful_web_service/src/main/java/com/laiszig/consume_restful_web_service/ConsumeRestfulWebServiceApplication.java
@@ -1,13 +1,33 @@
 package com.laiszig.consume_restful_web_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConsumeRestfulWebServiceApplication {
 
+    private static final Logger log =
+            LoggerFactory.getLogger(ConsumeRestfulWebServiceApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(ConsumeRestfulWebServiceApplication.class, args);
+    }
+
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://quoters.apps.pcfone.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
     }
 
 }
