e00f39fe4c6d635076fee914e2a28094b27070f1
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-10-30 19:27:28 -0300

    create getBook by id endpoint

--- books_restful/src/main/java/com/laiszig/book_restful/BookController.java
+++ books_restful/src/main/java/com/laiszig/book_restful/BookController.java
@@ -1,9 +1,6 @@
 package com.laiszig.book_restful;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,5 +21,15 @@ public class BookController {
         books.add(book);
         book.setId(id++);
         return book;
+    }
+
+    @GetMapping("/books/{id}")
+    public Book getBook(@PathVariable int id) {
+        for (int i = 0; i < books.size(); i++) {
+            if (id == books.get(i).getId()) {
+                return books.get(i);
+            }
+        }
+        return null; //Temp
     }
 }
