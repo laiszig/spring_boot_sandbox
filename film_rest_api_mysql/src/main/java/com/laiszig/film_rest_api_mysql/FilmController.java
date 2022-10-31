6c047d3d0aa1b7d61190012d3e2cf667269e4a62
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-10-31 13:14:31 -0300

    create Film repository

--- film_rest_api_mysql/src/main/java/com/laiszig/film_rest_api_mysql/FilmController.java
+++ film_rest_api_mysql/src/main/java/com/laiszig/film_rest_api_mysql/FilmController.java
@@ -1,22 +1,22 @@
 package com.laiszig.film_rest_api_mysql;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class FilmController {
+public class FilmController  {
 
     private final List<Film> films = new ArrayList<>();
 
+    @Autowired
+    private FilmRepository filmRepository;
+
     @GetMapping("/films")
     public List<Film> getFilms() {
-        return films;
+        return filmRepository.findAll();
     }
 }
---
+++ film_rest_api_mysql/src/main/java/com/laiszig/film_rest_api_mysql/FilmRepository.java
@@ -0,0 +1,6 @@
+package com.laiszig.film_rest_api_mysql;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilmRepository extends JpaRepository<Film, Integer> {
+}
