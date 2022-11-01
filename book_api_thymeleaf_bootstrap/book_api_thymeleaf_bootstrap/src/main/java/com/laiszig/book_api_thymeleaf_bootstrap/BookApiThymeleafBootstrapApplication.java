48b44f05cdabd79e04b445556d8521be76460d55
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-11-01 07:42:31 -0300

    build project, configure db, code home page

---
+++ book_api_thymeleaf_bootstrap/book_api_thymeleaf_bootstrap/src/main/java/com/laiszig/book_api_thymeleaf_bootstrap/BookApiThymeleafBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.book_api_thymeleaf_bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApiThymeleafBootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookApiThymeleafBootstrapApplication.class, args);
+    }
+
+}
---
+++ book_api_thymeleaf_bootstrap/book_api_thymeleaf_bootstrap/src/test/java/com/laiszig/book_api_thymeleaf_bootstrap/BookApiThymeleafBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.book_api_thymeleaf_bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiThymeleafBootstrapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
