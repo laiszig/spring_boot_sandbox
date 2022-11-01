ab0e7bb1e573d8cc2c888a51aed3b32b43148f00
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-11-01 08:10:33 -0300

    configure dev tools, bootstrap and create entity class

---
+++ book_api_thymeleaf_bootstrap/book_api_thymeleaf_bootstrap/src/main/java/com/laiszig/book_api_thymeleaf_bootstrap/Book.java
@@ -0,0 +1,53 @@
+package com.laiszig.book_api_thymeleaf_bootstrap;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String title;
+
+    @Column(nullable = false, length = 45)
+    private String author;
+
+    @Column(nullable = false, length = 45)
+    private String year;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
---
+++ book_api_thymeleaf_bootstrap/book_api_thymeleaf_bootstrap/src/main/java/com/laiszig/book_api_thymeleaf_bootstrap/BookRepository.java
@@ -0,0 +1,4 @@
+package com.laiszig.book_api_thymeleaf_bootstrap;
+
+public interface BookRepository {
+}
