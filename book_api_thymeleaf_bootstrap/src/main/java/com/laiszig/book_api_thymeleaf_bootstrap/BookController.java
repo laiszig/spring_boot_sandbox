9a76606832fcbc3a327ee5eefd455152a1f1f8b7
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-11-01 15:37:53 -0300

    create getAll endpoint, include books table and html doc

---
+++ book_api_thymeleaf_bootstrap/src/main/java/com/laiszig/book_api_thymeleaf_bootstrap/BookController.java
@@ -0,0 +1,29 @@
+package com.laiszig.book_api_thymeleaf_bootstrap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/books")
+    public String getAllBooks(Model model) {
+        List<Book> books = bookService.listAll();
+        model.addAttribute("books", books);
+
+        return "books";
+    }
+
+    @GetMapping("/books/new")
+    public String showNewForm(Model model){
+        model.addAttribute("book", new Book());
+        return "book_form";
+    }
+}
---
+++ book_api_thymeleaf_bootstrap/src/main/java/com/laiszig/book_api_thymeleaf_bootstrap/BookService.java
@@ -0,0 +1,17 @@
+package com.laiszig.book_api_thymeleaf_bootstrap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> listAll() {
+        return (List<Book>) bookRepository.findAll();
+    }
+}
