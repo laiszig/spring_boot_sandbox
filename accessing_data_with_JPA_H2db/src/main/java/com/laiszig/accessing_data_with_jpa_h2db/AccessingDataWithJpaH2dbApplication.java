f373089e272424562586036b0bc40a9aa398a8f1
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-11-08 10:30:09 -0300

    create spring project and add entity

---
+++ accessing_data_with_JPA_H2db/src/main/java/com/laiszig/accessing_data_with_jpa_h2db/AccessingDataWithJpaH2dbApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.accessing_data_with_jpa_h2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataWithJpaH2dbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataWithJpaH2dbApplication.class, args);
+    }
+
+}
