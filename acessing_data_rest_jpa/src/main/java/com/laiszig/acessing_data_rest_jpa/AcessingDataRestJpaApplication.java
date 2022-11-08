53e91d1b6e961363f42634faa699567909187bc9
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-11-08 14:38:18 -0300

    create entity and repository

---
+++ acessing_data_rest_jpa/src/main/java/com/laiszig/acessing_data_rest_jpa/AcessingDataRestJpaApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.acessing_data_rest_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessingDataRestJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcessingDataRestJpaApplication.class, args);
+    }
+
+}
---
+++ acessing_data_rest_jpa/src/test/java/com/laiszig/acessing_data_rest_jpa/AcessingDataRestJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.acessing_data_rest_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcessingDataRestJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
