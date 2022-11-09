b9359e80ed4b7233f4b0b42098dda76edd9b575b
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-11-09 14:08:30 -0300

    store and retrieve data logic using jdbc

---
+++ relational_data_spring_JDBC/src/test/java/com/laiszig/relational_data_spring_jdbc/RelationalDataSpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.relational_data_spring_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalDataSpringJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
