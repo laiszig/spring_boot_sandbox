341394490fb18104b4880a133c654d067cac7959
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-11-10 15:54:02 -0300

    create project and package structure

---
+++ spring_rest_projects/airline_rest_api/src/test/java/com/laiszig/airline_rest_api/AirlineRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.airline_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
