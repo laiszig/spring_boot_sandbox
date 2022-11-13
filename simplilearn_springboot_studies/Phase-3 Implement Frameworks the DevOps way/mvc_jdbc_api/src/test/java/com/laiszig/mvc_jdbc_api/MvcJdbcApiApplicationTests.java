7e1d61150149972ba3bbe6ff6092dcaaad9de147
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-11-13 20:16:27 -0300

    13.11.22 class project

---
+++ simplilearn_springboot_studies/Phase-3 Implement Frameworks the DevOps way/mvc_jdbc_api/src/test/java/com/laiszig/mvc_jdbc_api/MvcJdbcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.mvc_jdbc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcJdbcApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
