bce3d719d938a661e7c35dfe65f90454ad1b07a3
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-11-21 19:15:02 -0300

    repo update

--- simplilearn_springboot_studies/Phase-3 Implement Frameworks the DevOps way/sat11192022_event_handling/src/main/java/com/laiszig/sat11192022_event_handling/AppListener.java
+++ simplilearn_springboot_studies/Phase-3 Implement Frameworks the DevOps way/sat11192022_event_handling/src/main/java/com/laiszig/sat11192022_event_handling/AppListener.java
@@ -1,2 +1,26 @@
-package com.laiszig.sat11192022_event_handling;public class AppListener {
+package com.laiszig.sat11192022_event_handling;
+
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.ContextStoppedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppListener {
+
+    @EventListener
+    public void onApplicationEvent(ContextStartedEvent cse){
+        System.out.println("Context started !!!!");
+    }
+
+    @EventListener
+    public void onApplicationEvent1(ContextStartedEvent cse){
+        System.out.println("Context started $$$$");
+    }
+
+    @EventListener
+    public void onContextStopped(ContextStoppedEvent cse){
+        System.out.println("Context started &&&&");
+    }
+
 }
--- simplilearn_springboot_studies/Phase-3 Implement Frameworks the DevOps way/sun11132022_thymeleaf_mvc/src/main/java/com/laiszig/sun11132022_thymeleaf_mvc/controller/GreetController.java
+++ simplilearn_springboot_studies/Phase-3 Implement Frameworks the DevOps way/sun11132022_thymeleaf_mvc/src/main/java/com/laiszig/sun11132022_thymeleaf_mvc/controller/GreetController.java
@@ -3,13 +3,14 @@ package com.laiszig.sun11132022_thymeleaf_mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetController {
 
     @GetMapping("/greet")
-    public String greet(Model model) {
-        model.addAttribute("name", "Lais");
+    public String greet(Model model, @RequestParam("name") String name) {
+        model.addAttribute("name", name);
         return "hello";
     }
 }
