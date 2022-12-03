f971387a3842782b6965620b005e2a1ad1b0ff95
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-12-03 14:13:46 -0300

    create entities

--- spring_data_jpa_transaction_management/src/main/java/com/laiszig/spring_data_jpa_transaction_management/entity/PaymentInfo.java
+++ spring_data_jpa_transaction_management/src/main/java/com/laiszig/spring_data_jpa_transaction_management/entity/PaymentInfo.java
@@ -1,4 +1,28 @@
 package com.laiszig.spring_data_jpa_transaction_management.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "PAYMENT_INFO")
 public class PaymentInfo {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private String paymentId;
+    private String accountNo;
+    private Double amount;
+    private String cardType;
+    private Long passengerId;
+
 }
