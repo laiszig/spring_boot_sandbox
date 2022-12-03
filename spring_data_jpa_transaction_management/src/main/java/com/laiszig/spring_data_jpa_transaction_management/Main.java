8a5444a6c2f9127d1d8a2c32f747f9f0606ea26d
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-12-03 13:07:07 -0300

    create flight service project

---
+++ spring_data_jpa_transaction_management/src/main/java/com/laiszig/spring_data_jpa_transaction_management/Main.java
@@ -0,0 +1,13 @@
+package com.laiszig.spring_data_jpa_transaction_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaTransactionManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaTransactionManagementApplication.class, args);
+    }
+
+}
---
+++ spring_data_jpa_transaction_management/src/main/java/com/laiszig/spring_data_jpa_transaction_management/entity/PassengerInfo.java
@@ -0,0 +1,4 @@
+package com.laiszig.spring_data_jpa_transaction_management.entity;
+
+public class PassengerInfo {
+}
