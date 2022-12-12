2e1d138e7af675f14c185820236ed88f5e27ba62
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-12-12 16:56:05 -0300

    initial commit

---
+++ spring_rest_actuator/src/main/java/com/laiszig/spring_rest_actuator/SpringRestActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.spring_rest_actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestActuatorApplication.class, args);
+    }
+
+}
---
+++ spring_rest_actuator/src/test/java/com/laiszig/spring_rest_actuator/SpringRestActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.spring_rest_actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
