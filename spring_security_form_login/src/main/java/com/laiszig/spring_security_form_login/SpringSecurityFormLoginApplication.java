b9830b33048c61c10d570335d1ceec88c3aa262d
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-12-11 22:21:32 -0300

    initial commit

---
+++ spring_security_form_login/src/main/java/com/laiszig/spring_security_form_login/SpringSecurityFormLoginApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.spring_security_form_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFormLoginApplication {
+
+    public static void main(String[] args) throws Throwable{
+        SpringApplication.run(SpringSecurityFormLoginApplication.class, args);
+    }
+
+}
