530345dac824b5619faa9a91393b67073208ec52
Laís Ziegler <79112829+laiszig@users.noreply.github.com>
2022-12-13 21:46:58 -0300

    spring security integrated with jdbc

---
+++ spring_security_jdbc/src/test/java/com/laiszig/spring_security_form_login/SpringSecurityFormLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.spring_security_form_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFormLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
