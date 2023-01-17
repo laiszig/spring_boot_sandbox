56b6ceb51eac977f406e558268883354d532b666
laiszig <laisziegler@gmail.com>
2023-01-17 10:03:19 -0300

    create project structure

---
+++ resful_exception_handling/src/main/java/com/laiszig/resful_exception_handling/ResfulExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.resful_exception_handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResfulExceptionHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResfulExceptionHandlingApplication.class, args);
+    }
+
+}
---
+++ resful_exception_handling/src/test/java/com/laiszig/resful_exception_handling/ResfulExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.resful_exception_handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResfulExceptionHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
