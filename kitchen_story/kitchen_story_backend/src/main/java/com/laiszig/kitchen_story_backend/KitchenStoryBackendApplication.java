41e6b04a1d97644bd8845c4ec3293808f73b5a4e
laiszig <laisziegler@gmail.com>
2023-02-13 09:04:14 -0300

    initial commit

---
+++ kitchen_story/kitchen_story_backend/src/main/java/com/laiszig/kitchen_story_backend/KitchenStoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.kitchen_story_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenStoryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenStoryBackendApplication.class, args);
+    }
+
+}
---
+++ kitchen_story/kitchen_story_backend/src/test/java/com/laiszig/kitchen_story_backend/KitchenStoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.kitchen_story_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenStoryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
