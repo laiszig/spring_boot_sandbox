8e21be00a2d73351431ff5428c5e9080314cef6f
Lais_Ziegler <lais.ziegler@dell.com>
2023-03-09 17:22:11 -0300

    initial commit

---
+++ spring_exception_handling/spring_error_handling/src/test/java/com/laiszig/spring_error_handling/SpringErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.spring_error_handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
