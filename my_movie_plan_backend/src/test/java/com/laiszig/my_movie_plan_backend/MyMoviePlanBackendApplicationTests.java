ad35cffb0e2d7f51ca0ea44c4e61b5713128acbe
laiszig <laisziegler@gmail.com>
2023-03-19 10:19:32 -0300

    initial commit

---
+++ my_movie_plan_backend/src/test/java/com/laiszig/my_movie_plan_backend/MyMoviePlanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.my_movie_plan_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoviePlanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
