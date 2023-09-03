4e4f77752b1609dae1addaa9d0e56e0cc6f5fa6e
lais <laisziegler@gmail.com>
2023-09-03 18:05:29 -0300

    fallback

--- spring-gateway/src/main/java/com/laiszig/springgateway/SpringGatewayApplication.java
+++ spring-gateway/src/main/java/com/laiszig/springgateway/SpringGatewayApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
+@RestController
 @SpringBootApplication
 public class SpringGatewayApplication {
 
@@ -32,8 +36,15 @@ public class SpringGatewayApplication {
                         .uri("http://httpbin.org:80"))
                 .route(p -> p
                         .host("*.circuitbreaker.com")
-                        .filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
-                        .uri("http://httpbin.org:80")).
-                build();
+                        .filters(f -> f.circuitBreaker(config -> config
+                                .setName("mycmd")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
     }
 }
