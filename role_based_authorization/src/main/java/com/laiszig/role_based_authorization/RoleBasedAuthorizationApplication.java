a01d5e12d4b8a2562a630d4ff31f7f227158deac
lais <laisziegler@gmail.com>
2023-10-24 14:12:05 -0300

    set up db and entities

---
+++ role_based_authorization/src/main/java/com/laiszig/role_based_authorization/RoleBasedAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.role_based_authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoleBasedAuthorizationApplication.class, args);
+    }
+
+}
---
+++ role_based_authorization/src/test/java/com/laiszig/role_based_authorization/RoleBasedAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.role_based_authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
