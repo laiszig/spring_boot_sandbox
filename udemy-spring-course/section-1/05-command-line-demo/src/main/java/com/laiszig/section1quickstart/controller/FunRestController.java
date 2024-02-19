77ffcad9b8ffff90b208b54d4ba0d94e4159c255
lais <laisziegler@gmail.com>
2024-02-19 07:42:01 -0300

    command line

--- udemy-spring-course/section-1/05-command-line-demo/src/main/java/com/laiszig/section1quickstart/controller/FunRestController.java
+++ udemy-spring-course/section-1/05-command-line-demo/src/main/java/com/laiszig/section1quickstart/controller/FunRestController.java
@@ -1,7 +1,6 @@
 package com.laiszig.section1quickstart.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,11 +12,6 @@ public class FunRestController {
 
     @Value("${team.name}")
     private String teamName;
-
-    @GetMapping("/teaminfo")
-    public String getTeamInfo(){
-        return "Coach: " + coachName + ", Team name: " + teamName;
-    }
 
 
 }
