cd1efe9c1a12c7a29e742f4d7f768ed0c8e45d9c
lais <laisziegler@gmail.com>
2024-06-07 19:35:51 -0300

    hello

--- princeton-algorithms-I/hello/Week-1/HelloGoodbye.java
+++ princeton-algorithms-I/hello/Week-1/HelloGoodbye.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
-
 public class HelloGoodbye {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        // Using System.console() to read input from the user
+        java.io.Console console = System.console();
+
+        if (console == null) {
+            System.err.println("No console available");
+            return;
+        }
 
         // Prompt the user for the first name
-        System.out.print("Enter the first name: ");
-        String name1 = scanner.nextLine();
+        String name1 = console.readLine("Enter the first name: ");
 
         // Prompt the user for the second name
-        System.out.print("Enter the second name: ");
-        String name2 = scanner.nextLine();
+        String name2 = console.readLine("Enter the second name: ");
 
         // Print the hello message
         System.out.println("Hello " + name1 + " and " + name2 + ".");
@@ -19,3 +21,5 @@ public class HelloGoodbye {
         System.out.println("Goodbye " + name2 + " and " + name1 + ".");
     }
 }
+
+
