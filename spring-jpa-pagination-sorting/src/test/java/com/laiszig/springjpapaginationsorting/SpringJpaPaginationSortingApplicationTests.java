897ff3a2a1fd62122bce6f1faa5d1b943be65d32
laiszig <laisziegler@gmail.com>
2024-07-12 09:44:47 -0300

    initial commit

---
+++ spring-jpa-pagination-sorting/src/test/java/com/laiszig/springjpapaginationsorting/SpringJpaPaginationSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.springjpapaginationsorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaPaginationSortingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
